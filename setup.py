e1d36cb008d8758ba4b6468af9fb99ef0dce3a65
Bruno Alves <bruno.afonso.alves@tecnico.ulisboa.pt>
2018-10-07 12:23:24 +1100

    package structure updated

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
-#
+from distutils.core import setup
+
+setup(
+    name = 'dmprofile',
+    packages = ['dmprofile'],
+    version = 'v0.1.0',
+    description = 'pynbody wrapper to facilitate standard analysis of dark matter halos',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/b-fontana/DarkMatter',
+    download_url = 'https://github.com/b-fontana/DarkMatter/archive/v0.1.0.tar.gz',
+    keywords = ['dark matter', 'halos', 'pynbody'],
+    classifiers = [],
+)
