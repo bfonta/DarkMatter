deb72f56fd5c47d9b985416107999af75e2fd94c
Bruno Alves <bruno.afonso.alves@tecnico.ulisboa.pt>
2018-10-23 23:07:19 +1100

    i/o update; added remove_low_occupancy_bins

--- dmprofile/src/utilities.py
+++ dmprofile/src/utilities.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import pynbody as pn
+import h5py
 
 def rho_crit(sim):
     return pn.array.SimArray(pn.analysis.cosmology.rho_crit(sim, unit="Msol kpc**-3"), "Msol kpc**-3")
@@ -37,13 +38,114 @@ def linear_function_generator(p1, p2, left, right):
         r = random.uniform(left,right)
         yield r, r*p1 + p2
 
+def remove_low_occupancy_bins(h, nbins_min):
+    """
+    Returns an array representing the edges of the bins of the 'h' numpy.histogram
+    where the bins containing less than 'nbins_min' samples were removed.
+    It only removes bins at the edges. If a bins has less than 'nbins_min' samples but lies between
+    bins that have 'nbins_min' samples or more it will not be removed.
+    If many consecutive bins have less than 'nbins_min' samples and are at the edge, 
+    all will be removed.
+    """
+    bin_samples = h[0]
+    bin_edges = h[1]
+    for i in reversed(range(len(bin_samples))):
+        flag = False
+        if bin_samples[i]<3:
+            flag = True
+            bin_edges = bin_edges[:-1]
+        if flag==False:
+            break
+    for i in range(len(bin_samples)):
+        flag = False
+        if bin_samples[i]<3:
+            flag = True
+            bin_edges = bin_edges[1:]
+        if flag==False:
+            break
+    return bin_edges
+
 def memory_usage_resource():
     import os
     import psutil
     _process = psutil.Process(os.getpid())
     print(_process.memory_info().rss / float(2**20), "MiB being used.")
 
-def write_to_file(fname, arr1, arr2):
-    with open(fname, 'w') as f:
-        for x, y in zip(arr1,arr2):
-            f.write('{},{}\n'.format(x,y))
+def write_to_file(fname, *args, mode='normal', hdf5_shape=(0,0)):
+    """
+    Write to files a length variable group of arrays.
+    Works both with numpy arrays and lists.
+    """
+    modes = ['normal','hdf5']
+    if mode not in modes:
+        raise ValueError('The specified mode is not supported.')
+    if mode=='hdf5' and hdf5_shape==(0,0,0):
+        raise ValueError('Please specify a valid shape for the hdf5 dataset.')
+    if mode=='hdf5' and fname[-5:]!='.hdf5':
+        raise ValueError('Please specify an appropriate name for a hdf5 file.')
+    if len(hdf5_shape)!=2:
+        raise TypeError('Currently only 2D hdf5 datasets can be stored.')
+    if len(args)<2:
+        raise ValueError('At least to arrays must be written to the file.')
+    if not type(fname)==str:
+        raise TypeError('The first argument must be a string, and represents the name of the file.')
+    for arg in args:
+        if not isinstance(arg, (list, np.ndarray)):
+            raise TypeError('Except for the file name, all other arguments must be numpy arrays.')
+
+    if mode=='hdf5':
+        with h5py.File(fname, 'w') as f:
+            dset = f.create_dataset('shape_dataset', hdf5_shape)
+            counter = 0
+            for x, *data in zip(*args):
+                print("TYPE::", x)
+                print("ID:", counter)
+                dset[counter,0]=x[1][0]
+                dset[counter,1]=x[2][0]
+                dset[counter,2]=data[0]
+                counter += 1
+    else:
+        with open(fname, 'w') as f:    
+            for x, *data in zip(*args):
+                blanks = '{},'*len(args)+'\n'
+                f.write('{},'.format(x))
+                for i in range(len(args)-1):
+                    if i==len(args)-2:
+                        f.write('{}\n'.format(data[i]))
+                    else:
+                        f.write('{},'.format(data[i]))
+
+def read_from_file(fname, splitter=',', mode='normal'):
+    modes = ['normal','hdf5']
+    if mode not in modes:
+        raise ValueError('The specified mode is not supported.')
+
+    if mode=='hdf5':
+        ba, ca, extra = ([] for i in range(3))
+        with h5py.File(fname, 'r') as f:
+            dset = f['shape_dataset']
+            counter = 0
+            for i in range(dset.shape[0]):
+                ba.append(dset[i,0])
+                ca.append(dset[i,1])
+                extra.append(dset[i,2])
+        return ba, ca, extra
+
+    else:
+        with open(fname) as f:
+            for values in f:
+                values = values.split(splitter)
+                ncols = len(values)
+                break
+            data = [[] for i in range(ncols)]
+            with open(fname) as f:
+                for values in f:
+                    values = values.replace('\n','').split(splitter)
+                    for i,val in enumerate(values):
+                        if val=='False':
+                            data[i].append(False)
+                        elif val=='True':
+                            data[i].append(True)
+                        else:
+                            data[i].append(float(val))
+        return [np.array(data[i]) for i in range(ncols)]
