84454368d4a9a1a9896c77f0020b8165df7bce6a
Bruno Alves <bruno.afonso.alves@tecnico.ulisboa.pt>
2019-02-20 15:45:04 +1100

    script plot

--- script_plot.py
+++ script_plot.py
@@ -5,6 +5,8 @@ from dmprofile.src import plot
 from dmprofile.src.utilities import read_from_file as rf
 from dmprofile.src.parser import add_args
 
+#run with: python script_plot.py --sim_min_particle_number 500 --sim_sizes 512 --redshift 5
+
 FLAGS, _ = add_args(argparse.ArgumentParser())
 print("Parsed arguments:")
 for k,v in FLAGS.__dict__.items():
@@ -12,22 +14,25 @@ for k,v in FLAGS.__dict__.items():
 
 st = FLAGS.sim_types
 min_size = FLAGS.sim_min_particle_number
-sim_sizes = FLAGS.sim_sizes[0]
-redshift_dict = {5: '103', 6: '080', 7: '065', 8: '054', 9: '045'}
-rshift = redshift_dict[FLAGS.redshift]
+sim_size = FLAGS.sim_sizes[0]
+#redshift_dict = {5: '103', 6: '080', 7: '065', 8: '054', 9: '045'}
+#rshift = redshift_dict[FLAGS.redshift]
+rshift = FLAGS.redshift
 
-_c = [rf('data/Concentration_'+st[i]+'_'+str(min_size)+'_'+sim_sizes+'_redshift'+rshift+'.txt') 
-      for i in range(len(st))]
+#Retrieve all values and perform mass selection
+_c = [rf('data2/Concentration_'+st[i]+'_'+str(min_size)+'_'+sim_size+'_redshift'+str(rshift)+'.txt') for i in range(len(st))]
 
 c, M200, res, rel = ([[] for _ in range(len(st))] for _ in range(4))
 for isim in range(len(st)):
-    c[isim] = _c[isim][0]
-    M200[isim] = np.log10(_c[isim][1])
+    bool_arr = np.array([True if m>=8. else False for m in np.log10(_c[isim][1])])
+    c[isim] = np.array(_c[isim][0])[bool_arr]
+    M200[isim] = np.log10(_c[isim][1])[bool_arr]
     #res[isim] = _c[isim][2]
     #rel[isim] = _c[isim][3]
 
+#Plotting
 c_obj = plot.Concentration(c, extra_var=M200, nrows=1, ncols=1, 
-                           name='figs/Concentration'+str(FLAGS.redshift)+'.png', h=8, w=7)
+                           name='figs2/Concentration'+str(FLAGS.redshift)+'.png', h=8, w=7)
 c_obj.set_all_properties(model='concentration_mass_log')
 
 names_dict = {'DMONLY': 'DMONLY', 
@@ -45,6 +50,7 @@ for isim in range(len(st)):
                       label = names_dict[st[isim]], 
                       color = colors_dict[st[isim]],
                       n_resampling=10000,
-                      xscale='linear', xlim=[6.8,11.2],
-                      extra_idx=isim, fit=False)
+                      xscale='linear', xlim=[7.8,11.2],
+                      extra_idx=isim,
+                      fit_func=lambda x,a,b,c: a * ((x/9)**b) * (((1+rshift)/8)**c))
 c_obj.savefig()
